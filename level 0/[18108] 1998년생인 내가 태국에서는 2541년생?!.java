7d95d38ed857409fa0ef6e0641107a234bc9dae5
Hyewon <hooho931@gmail.com>
2023-11-05 23:09:14 +0900

    Create [18108] 1998년생인 내가 태국에서는 2541년생?!.java

---
+++ level 0/[18108] 1998년생인 내가 태국에서는 2541년생?!.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner y = new Scanner(System.in);
+    int year = y.nextInt();
+      
+    System.out.println(year-543);
+  }
+}
