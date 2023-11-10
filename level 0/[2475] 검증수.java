5b130a85fe97afe509cd4cfe552dffe816860f2b
Hyewon <hooho931@gmail.com>
2023-11-10 23:05:04 +0900

    Create [2475] 검증수.java

---
+++ level 0/[2475] 검증수.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args)  {
+    Scanner sc = new Scanner(System.in);
+    int[] arr = new int[5];
+    int sum=0;
+    
+    for(int i=0; i<arr.length; i++){
+        arr[i] = sc.nextInt();
+        sum += arr[i] * arr[i];
+    }
+    System.out.printf("%d", sum%10);
+  }
+}
