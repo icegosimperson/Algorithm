a290cae4a19838c27c43b83141c6b31b93a627d2
Hyewon <hooho931@gmail.com>
2023-11-11 20:38:26 +0900

    Create [15964] 이상한 기호.java

---
+++ level 0/[15964] 이상한 기호.java
@@ -0,0 +1,14 @@
+// Long형 주의!
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args)  {
+    Scanner sc = new Scanner(System.in);
+    long A = sc.nextLong();
+    long B = sc.nextLong();
+    
+    System.out.print((A+B)*(A-B));
+      
+    
+  }
+}
