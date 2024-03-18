ff0c5ed264d9f59a4fc6544e38355727a1eb2bf8
Hyewon <hooho931@gmail.com>
2024-03-18 14:05:48 +0900

    Create [9086] 문자열.java

---
+++ level 0/[9086] 문자열.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<T; i++) {
+        	String st = br.readLine();
+            bw.write(st.charAt(0) + "" + st.charAt(st.length()-1) + "\n");
+        }
+
+
+       br.close();
+       bw.flush();
+       bw.close();
+       }
+    
+}
