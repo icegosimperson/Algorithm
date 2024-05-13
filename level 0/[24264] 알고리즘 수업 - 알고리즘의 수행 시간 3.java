7c4dd72cfdbc453712472c42789cbfa726e97711
Hyewon <hooho931@gmail.com>
2024-05-13 13:13:15 +0900

    Create [24264] 알고리즘 수업 - 알고리즘의 수행 시간 3.java

---
+++ level 0/[24264] 알고리즘 수업 - 알고리즘의 수행 시간 3.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main{
+	 
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       
+        long n = Long.parseLong(br.readLine());
+        br.close();
+        
+        bw.write(String.valueOf(n*n+"\n"));
+        bw.write("2");
+        
+
+        
+        bw.flush();
+        bw.close();
+    }
+}
