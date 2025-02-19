f657ab84f3f48a3878ecb564514201b7efeabf1a
icegosimperson <hooho931@gmail.com>
2025-02-19 21:40:31 +0900

    [BOJ] 10824 네 수_240217

---
+++ 구현/BOJ_10824.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class BOJ_10824{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String A = st.nextToken();
+        String B = st.nextToken();
+        String C = st.nextToken();
+        String D = st.nextToken();
+
+        Long a = Long.parseLong(A+B);
+        Long b = Long.parseLong(C+D);
+
+        System.out.println(a+b);
+    }
+}
