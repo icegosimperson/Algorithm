51e751db0bcfec81bfd7ae634fbc88b146bb11b7
icegosimperson <hooho931@gmail.com>
2025-03-08 22:51:16 +0900

    [BOJ] 24883 자동완성_240308

---
+++ 구현/BOJ_24883.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class BOJ_24883{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        if(input.equals("N") || input.equals("n")){
+            System.out.println("Naver D2");
+        } else{
+            System.out.println("Naver Whale");
+        }
+
+    }
+}
