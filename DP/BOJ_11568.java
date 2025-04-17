48ce7ed8417928ad92ef529e6c93456c9c00d326
Hyewon Lee <hooho931@gmail.com>
2025-04-17 09:07:49 +0900

    [BOJ] 11568 민균이의 계략_250417

---
+++ DP/BOJ_11568.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+class BOJ_11568{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] dp = new int[N];
+
+        for(int i=0; i<N; i++){
+            dp[i] = 1;
+            for(int j=0; j<i; j++){
+                if(arr[i] > arr[j]){ // 이전값 보다 현재값이 크면
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                }
+            }
+        }
+        int LIS = 0;
+        for(int i=0; i<N; i++){
+            LIS = Math.max(LIS, dp[i]);
+        }
+
+        bw.write(String.valueOf(LIS));
+        bw.flush();
+        bw.close();
+    }
+}
