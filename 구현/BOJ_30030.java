db5057cfbd91b31fe460b5acb82d3e626fd6d240
Hyewon Lee <hooho931@gmail.com>
2025-04-20 21:18:57 +0900

    [BOJ] 30030 스위트콘 가격 구하기_250420

---
+++ 구현/BOJ_30030.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+
+class BOJ_30030{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int B = Integer.parseInt(br.readLine());
+        System.out.println((int)B*10/11);
+
+    }
+}
