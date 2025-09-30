fd3afbeb873381ba67bef41094bf439c3878281c
Hyewon Lee <hooho931@gmail.com>
2025-09-30 23:12:35 +0900

    [BOJ] 33701_250930

---
+++ level 0/BOJ_33701.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+// 강의실이 수용할 수 있는 최대 인원 수를 출력
+class BOJ_33701{
+    public static void main(String[] args) throws IOException {
+        System.out.print("nkugwan");
+    }
+
+}
