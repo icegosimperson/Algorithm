b7bcbc262a4b0f710249ec58937dea915f0d82dd
Hyewon Lee <hooho931@gmail.com>
2025-10-03 23:58:49 +0900

    [PG] 자연수 뒤집어 배열로 만들기_251003

---
+++ 문자열/PG_12932.java
@@ -0,0 +1,9 @@
+class PG_12932 {
+    public int[] solution(long n) {
+        String str = String.valueOf(n);
+        int[] answer = new int[str.length()];
+        for(int i=0; i<str.length();i++){
+            answer[i] = str.charAt(str.length()-1-i) - '0';
+        }
+        return answer;
+    }
