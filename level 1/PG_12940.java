b7670b8bd63b7dae4cdf3361d0eba82f482d676b
Hyewon Lee <hooho931@gmail.com>
2025-10-21 22:47:57 +0900

    [PG] level1 (3)_251021

---
+++ level 1/PG_12940.java
@@ -0,0 +1,15 @@
+// [최대 공약수, 최소 공배수]
+import java.util.*;
+
+class PG_12940 {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        for(int i=1; i<=Math.max(n, m); i++){
+            if(n%i==0 && m%i==0){
+                answer[0] = i;
+            }
+        }
+        answer[1] = n*m / answer[0];
+        return answer;
+    }
+}
