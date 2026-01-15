9e826ed5d4f6f64adfc863068c313d9804019877
Hyewon Lee <hooho931@gmail.com>
2026-01-15 18:50:28 +0900

    [PG] level1 +1_260115

---
+++ level 1/PG_310198_R2.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class PG_310198_R2 {
+    public int solution(int[] mats, String[][] park) {
+        Arrays.sort(mats);
+        for(int i=mats.length-1; i>=0; i--){
+            int size = mats[i];
+            if(canPlace(size, park)){
+                return size;
+            }
+        }
+        return -1;
+    }
+    private static boolean canPlace(int size, String[][] park){
+        int r = park.length; int c = park[0].length;
+        for(int i=0; i<=r-size; i++){
+            for(int j=0; j<=c-size; j++){
+                if(isEmpty(i, j, size, park)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private static boolean isEmpty(int r, int c, int size, String[][] park){
+        for(int i=r; i<r+size; i++){
+            for(int j=c; j<c+size; j++){
+                if(!park[i][j].equals("-1")){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
