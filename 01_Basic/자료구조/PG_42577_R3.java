ba870c205d8752ac34a6ac2c9778a12d247c3514
Hyewon Lee <hooho931@gmail.com>
2026-03-01 17:11:32 +0900

    [solved] (PG Lv2) 전화번호 목록_260301

---
+++ 01_Basic/자료구조/PG_42577_R3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class PG_42577_R3 {
+    public boolean solution(String[] phone_book) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String p : phone_book){
+            map.put(p, map.getOrDefault(p, 0)+1);
+        }
+        for(String p : phone_book){
+            for(int i=1; i<p.length(); i++){
+                if(map.containsKey(p.substring(0, i))){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
