1476907c93e938ebcb8b2de2a907eda487ea2cf0
Hyewon Lee <hooho931@gmail.com>
2026-03-02 11:45:50 +0900

    [review] (PG Lv2) 의상_260302

---
+++ 01_Basic/자료구조/PG_42578_R3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class PG_42578_R3 {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String[] c : clothes){
+            map.put(c[1], map.getOrDefault(c[1], 0)+1);
+        }
+        // ( , 1) ( , 1) ( , 1)
+        for(int cnt : map.values()){
+            answer *= cnt+1;
+        }
+        return answer-1;
+    }
+}
