c27d88024df976419519fcd6f98affc18f61bc5d
Hyewon Lee <hooho931@gmail.com>
2026-03-11 17:13:52 +0900

    [solved] (PG Lv2) 구명보트_260311

---
+++ 02_Focus/그리디/PG_42885_R3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class PG_42885_R3 {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int idx = 0;
+        Arrays.sort(people);
+        for(int i=people.length-1; i>=idx; i--){
+            if(people[i] + people[idx] <= limit){
+                idx++;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
