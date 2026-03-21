10c62de8b9cf7a2984d4ccca1cfa599dc03bfdc0
Hyewon Lee <hooho931@gmail.com>
2026-03-21 14:40:05 +0900

    [solved] (PG Lv2) 연속 부분 수열 합의 개수_260321

---
+++ 02_Focus/level2/PG_131701.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class PG_131701 {
+    public int solution(int[] elements) {
+        Set<Integer> set = new HashSet<>();
+        for(int i=0; i<elements.length; i++){
+            int sum = 0;
+            for(int j=0; j<elements.length; j++){
+                sum += elements[(i+j)%elements.length];
+                set.add(sum);
+            }
+        }
+        return set.size();
+    }
+}
