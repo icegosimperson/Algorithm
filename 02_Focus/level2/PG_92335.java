e61519cc7b9107898c482213cb5b4413796799ac
Hyewon Lee <hooho931@gmail.com>
2026-04-20 20:00:22 +0900

    [solved] (PG Lv2) k진수에서 소수 개수 구하기_260420

--- 02_Focus/level2/PG_92335.java
+++ 02_Focus/level2/PG_92335.java
@@ -32,3 +32,25 @@ class PG_92335 {
         return sb.reverse().toString();
     }
 }
+/*
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        String[] split = Integer.toString(n, k).split("0");
+        for(String s : split){
+            if(s.isEmpty()) continue;
+            if(isPrime(Long.parseLong(s))){
+                answer++;
+            }
+        }
+        return answer;
+    }
+    public boolean isPrime(long n){
+        if(n<2) return false;
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n%i==0) return false;
+        }
+        return true;
+    }
+}
+ */
