c253792c98e4785ec2ea35408ea30b57d7d9e113
Hyewon Lee <hooho931@gmail.com>
2026-04-23 11:21:59 +0900

    [review] (PG Lv2) 땅따먹기_260423

--- 02_Focus/level2/PG_12913.java
+++ 02_Focus/level2/PG_12913.java
@@ -17,3 +17,25 @@ class PG_12913 {
         return answer;
     }
 }
+/*
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        for(int i=1; i<land.length; i++){
+            for(int j=0; j<4; j++){ // 현재 선택
+                int max = 0;
+                for(int k=0; k<4; k++){ // 이전에 선택한 값
+                    if(j!=k){
+                        max = Math.max(land[i-1][k], max);
+                    }
+                }
+                land[i][j] += max;
+            }
+        }
+        for(int j=0; j<4; j++){
+            answer = Math.max(land[land.length-1][j], answer);
+        }
+        return answer;
+    }
+}
+ */
