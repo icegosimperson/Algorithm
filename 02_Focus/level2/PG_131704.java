6ea01b7cca506184bf3c26f33ce006595fcc2ac1
Hyewon Lee <hooho931@gmail.com>
2026-04-27 11:28:08 +0900

    [review] (PG Lv2) 택배상자_260427

--- 02_Focus/level2/PG_131704.java
+++ 02_Focus/level2/PG_131704.java
@@ -1,23 +1,40 @@
 import java.util.*;
 class PG_131704 {
     public int solution(int[] order) {
-        Stack<Integer> stack = new Stack<>();
         int answer = 0;
         int box = 1;
-        for(int i=0; i<order.length; i++){
-            if(!stack.isEmpty() && stack.peek()== order[i]){
-                answer++;
-                stack.pop();
-            } else{
-                while(box<order.length && box!=order[i]){
-                    stack.push(box++);
-                }
-                if(box==order[i]){
-                    answer++;
-                    box++;
-                } else break;
+        Stack<Integer> stack = new Stack<>();
+        for(int target : order){
+            while(box<=order.length && box<=target){
+                stack.push(box++);
             }
+            if(!stack.isEmpty() && stack.peek()==target){
+                stack.pop();
+                answer++;
+            } else break;
         }
         return answer;
     }
 }
+//class PG_131704 {
+//    public int solution(int[] order) {
+//        Stack<Integer> stack = new Stack<>();
+//        int answer = 0;
+//        int box = 1;
+//        for(int i=0; i<order.length; i++){
+//            if(!stack.isEmpty() && stack.peek()== order[i]){
+//                answer++;
+//                stack.pop();
+//            } else{
+//                while(box<order.length && box!=order[i]){
+//                    stack.push(box++);
+//                }
+//                if(box==order[i]){
+//                    answer++;
+//                    box++;
+//                } else break;
+//            }
+//        }
+//        return answer;
+//    }
+//}
