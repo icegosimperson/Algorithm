9a9d6cfc3349824eec1b2f423a527e248c033a37
Hyewon Lee <hooho931@gmail.com>
2026-04-30 09:56:56 +0900

    [solved] (PG Lv2) 2xn 타일링_260430

---
+++ 02_Focus/level2/PG_12900.java
@@ -0,0 +1,12 @@
+class PG_12900 { // 2xn 타일링
+    public int solution(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++){
+            dp[i] = (dp[i-2] + dp[i-1]) % 1_000_000_007;
+        }
+        return dp[n];
+    }
+}
