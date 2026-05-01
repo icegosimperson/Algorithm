df1d41b7efa71715cd2469d35458b8d3eccd1040
Hyewon Lee <hooho931@gmail.com>
2026-05-01 11:12:57 +0900

    [review] Rank scores_260501

--- 02_Focus/SQL/03_Advanced/Window_Function/Rank Scores.sql
+++ 02_Focus/SQL/03_Advanced/Window_Function/Rank Scores.sql
@@ -4,3 +4,9 @@ FROM (
         DENSE_RANK() OVER (ORDER BY score DESC) as rnk
     FROM Scores
 )sub;
+--SELECT *
+--FROM (
+--    SELECT score,
+--        DENSE_RANK () OVER (ORDER BY score DESC) as "rank"
+--    FROM Scores
+--)Scores;
