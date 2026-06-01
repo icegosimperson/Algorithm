a05dc49d126d651e616147cd86971f0502d28788
Hyewon Lee <hooho931@gmail.com>
2026-06-01 21:48:43 +0900

    [review] (PG Lv0) 배열 비교하기_260601

---
+++ 05_Review/A/R_181856.java
@@ -0,0 +1,11 @@
+import java.util.stream.*;
+import java.util.*;
+class R_181856 {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = Integer.compare(arr1.length, arr2.length);
+        if(answer==0){
+            answer = Integer.compare(IntStream.of(arr1).sum(), IntStream.of(arr2).sum());
+        }
+        return answer;
+    }
+}
