531b1e0003e181c17fe4145f769433c446f2ff28
Hyewon Lee <hooho931@gmail.com>
2026-06-19 23:11:11 +0900

    [solved] 1004. Max Consecutive Ones III_260619

---
+++ 01_Basic/자료구조/리트코드/1004_Max Consecutive Ones III.java
@@ -0,0 +1,21 @@
+class 1004_Max Consecutive Ones III.java {
+    public int longestOnes(int[] nums, int k) {
+        // O(N) 으로 k개를 1로 뒤집어서 가장 긴 길이 출력
+        int zero = 0;
+        int start = 0;
+        int max = 0;
+        for(int end = 0; end<nums.length; end++){
+            if(nums[end]==0){
+                zero++;
+            }
+            while(zero>k){
+                if(nums[start]==0){
+                    zero--;
+                }
+                start++;
+            }
+            max = Math.max(max, end-left+1);
+        }
+        return max;
+    }
+}
