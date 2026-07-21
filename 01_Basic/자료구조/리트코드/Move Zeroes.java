e682156bea351aed34b121152d40bbcb5a554470
Hyewon Lee <hooho931@gmail.com>
2026-07-21 12:34:57 +0900

    [solved] Move Zeroes_260721

---
+++ 01_Basic/자료구조/리트코드/Move Zeroes.java
@@ -0,0 +1,13 @@
+class Move Zeroes {
+    public void moveZeroes(int[] nums) {
+        int insertPos=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i] = nums[insertPos];
+                nums[insertPos] = temp;
+                insertPos++;
+            }
+        }
+    }
+}
