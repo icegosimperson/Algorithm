0dbfef36ae2da095865195752935016421a556be
Hyewon Lee <hooho931@gmail.com>
2026-07-24 07:02:34 +0900

    [solved] 125.Valid Palindrome_260724

---
+++ 01_Basic/자료구조/리트코드/125.Valid Palindrome.java
@@ -0,0 +1,24 @@
+class 125.Valid Palindrome {
+    public boolean isPalindrome(String s) {
+        if(s.isEmpty()){
+            return true;
+        }
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            char leftChar = Character.toLowerCase(s.charAt(left));
+            char rightChar = Character.toLowerCase(s.charAt(right));
+            if(!Character.isLetterOrDigit(leftChar)){
+                left++;
+            } else if(!Character.isLetterOrDigit(rightChar)){
+                right--;
+            } else if(leftChar!=rightChar){
+                return false;
+            } else{
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
