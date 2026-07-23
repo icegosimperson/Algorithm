671b6aa45a85c62a6dd4a72a66e820cb40c32c3f
Hyewon Lee <hooho931@gmail.com>
2026-07-24 07:59:05 +0900

    [solved] 8.String to Integer (atoi)_260724

---
+++ 01_Basic/자료구조/리트코드/8.String to Integer (atoi).java
@@ -0,0 +1,30 @@
+class 8.String to Integer (atoi){
+    public int myAtoi(String s) {
+        int i=0; int n=s.length();
+        while(i<n && s.charAt(i)==' '){
+            i++;
+        }
+        if(i==n) return 0;
+        int sign=1;
+        if(s.charAt(i)=='+' || s.charAt(i)=='-'){
+            sign = s.charAt(i)=='-' ? -1 : 1;
+            i++;
+        }
+        long result=0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            result = result * 10 + (s.charAt(i)-'0');
+            if(result>Integer.MAX_VALUE){
+                break;
+            }
+            i++;
+        }
+        result *= sign;
+        if(result>Integer.MAX_VALUE){
+            return Integer.MAX_VALUE;
+        }
+        if(result<Integer.MIN_VALUE){
+            return Integer.MIN_VALUE;
+        }
+        return (int) result;
+    }
+}
