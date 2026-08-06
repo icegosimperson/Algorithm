e5d8399c2db98091c1dac9daa91e572c900b00ba
Hyewon Lee <hooho931@gmail.com>
2026-08-06 10:35:50 +0900

    [solved] 461. Hamming Distance_260806

---
+++ 01_Basic/자료구조/리트코드/461. Hamming Distance.java
@@ -0,0 +1,12 @@
+class 461. Hamming Distance {
+    public int hammingDistance(int x, int y) {
+        // return Integer.bitCount(x^y);
+        int xor = (x^y);
+        int count=0;
+        while(xor!=0){
+            count += xor & 1;
+            xor>>=1;
+        }
+        return count;
+    }
+}
