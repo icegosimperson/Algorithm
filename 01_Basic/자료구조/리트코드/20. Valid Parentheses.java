1123364a018f3bb8eccf4d8e582a17385d94f4b6
Hyewon Lee <hooho931@gmail.com>
2026-08-07 21:43:10 +0900

    [solved] 20. Valid Parentheses_260807

---
+++ 01_Basic/자료구조/리트코드/20. Valid Parentheses.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class 20. Valid Parentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c=='(' || c=='{' || c=='['){
+                stack.push(c);
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                char top = stack.pop();
+                if(top=='(' && c!=')'){
+                    return false;
+                }
+                if(top=='[' && c!=']'){
+                    return false;
+                }
+                if(top=='{' && c!='}'){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
