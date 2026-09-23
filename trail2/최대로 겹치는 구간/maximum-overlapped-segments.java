c3bb780fcd709e3a82a60b1f8312b34403f12730
Hyewon <hooho931@gmail.com>
2026-09-23 17:33:59 +0900

    [Passed] 최대로 겹치는 구간 (133ms, 10MB)

---
+++ trail2/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x1 = new int[n];
+        int[] x2 = new int[n];
+        int[] arr = new int[201];
+        int max = 0;
+        for (int i = 0; i < n; i++) { 
+            x1[i] = sc.nextInt() + 100;
+            x2[i] = sc.nextInt() + 100;
+            for(int j=x1[i]; j<x2[i]; j++){
+                arr[j] += 1;
+                max = Math.max(arr[j], max);
+            }
+        }
+        // Please write your code here.
+        System.out.println(max);
+    }
+}
